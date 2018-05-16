81e2ec063dbdf8329ff67165ea57b88857aeaf9f
falcomcpherson <falco.mcpherson@gmail.com>
2018-05-15 20:53:42 -0400

    Update to add 2 pages and clean code

--- app/src/main/java/com/example/android/playindarts/GameRules.java
+++ app/src/main/java/com/example/android/playindarts/GameRules.java
@@ -6,36 +6,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
+public class GameRules extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-
-    Button goToRules;
     Button goToScoreTracker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game_rules);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        goToRules = findViewById(R.id.goToRules);
-        goToRules.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent goToRules = new Intent(MainActivity.this, GameRules.class);
-                startActivity(goToRules);
-            }
-        });
 
         goToScoreTracker = findViewById(R.id.goToScoreTracker);
         goToScoreTracker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToScoreTracker = new Intent(MainActivity.this, ScoreTracker.class);
+                Intent goToScoreTracker = new Intent(GameRules.this, ScoreTracker.class);
                 startActivity(goToScoreTracker);
             }
-        });
+       });
     }}
+
